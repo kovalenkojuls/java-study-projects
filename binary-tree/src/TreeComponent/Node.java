5653221b2c25fc2fc259a6e3d1d36cf5a583a762
kovalenkojuls <kovalenko_jul_s@mail.ru>
2024-05-07 13:44:11 +0300

    add binary tree

---
+++ binary-tree/src/TreeComponent/Node.java
@@ -0,0 +1,13 @@
+package TreeComponent;
+
+public class Node {
+    int nodeValue;
+    Node leftChild, rightChild;
+
+    public Node(int nodeValue) {
+        this.nodeValue = nodeValue;
+        leftChild = null;
+        rightChild = null;
+    }
+
+}

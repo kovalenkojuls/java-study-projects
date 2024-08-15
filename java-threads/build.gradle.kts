cfb468a6c2a36528f65d103ea4ac348d33f1224b
kovalenkojuls <kovalenko_jul_s@mail.ru>
2024-08-15 14:56:52 +0300

    add java threads

---
+++ java-threads/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java")
+}
+
+group = "ru.julia"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("ch.qos.logback:logback-classic:1.5.6")
+}
